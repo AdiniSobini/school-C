0cbd6a872b1f9e08e172dd92e81aa4d473626925
Adrian:) <86928451+AdiniSobini@users.noreply.github.com>
2023-11-27 15:38:01 +0000

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int dodawanie, kalkulator, a, b, odejmowanie, mnozenie, dzielenie, warunek_zakonczenia;
+	
+	
+	
+	do {
+		cout<<"Czesc jestem kalkulatorem"<<endl<<"Co chcesz zrobic???"<<endl<<"1. dodawanie	/ 2. odejmowanie / 3. mnozenie / 4. dzielenie" ;
+		cin>>kalkulator;
+	
+	
+		;switch(kalkulator){
+	
+
+	
+		case 1:
+			cout<<"Dodawanie";
+			cout<<"Podaj pierwsza liczbe ";
+			cin>>a;
+			cout<<"Podaj druga liczbe ";
+			cin>>b;
+			cout<<"Wynik ="<<a+b<<endl;
+			break;
+		case 2:
+			cout<<"Odejmowanie";
+			cout<<"Podaj pierwsza liczbe ";
+			cin>>a;
+			cout<<"Podaj druga liczbe ";
+			cin>>b;
+			cout<<"Wynik ="<<a-b<<endl;
+			break;
+        case 3:
+        cout<<"Mnozenie";
+			cout<<"Podaj pierwsza liczbe ";
+			cin>>a;
+			cout<<"Podaj druga liczbe ";
+			cin>>b;
+			cout<<"Wynik ="<<a*b<<endl;
+			break;
+        case 4:
+        cout<<"Dzielenie";
+			cout<<"Podaj pierwsza liczbe ";
+			cin>>a;
+			cout<<"Podaj druga liczbe ";
+			cin>>b;
+			cout<<"Wynik ="<<a/b<<endl;
+			break;
+		default:
+			cout<<"Error"<<endl;
+		}
+		
+		cout<<"Czy chcesz kontynuowaæ program? 1. Tak / 2. Nie";
+		cin>>warunek_zakonczenia;
+		}
+		while(warunek_zakonczenia);
+		
+		cout<<"Adrian_Sobczak";
+		return 0;
+		
+		}
+		
