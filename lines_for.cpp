82bd1578aaa05bb769c92ba2a1c8a328e5dbd902
Adrian:) <86928451+AdiniSobini@users.noreply.github.com>
2024-03-20 10:47:20 +0100

    Add files via upload

---
+++ lines_for.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+
+main() 
+{
+
+
+
+
+
+
+
+cout << "welcome in my program I will show program for" <<endl;
+
+
+for(int i = 0;i<=3;i++) 
+
+			{
+
+			cout<<"*"<<endl;
+
+			}
+
+			for(int i=0;i<=5;i++)
+
+			{
+
+			cout<<"*";
+
+			}
+
+			cout<<endl;
+
+			cout<<endl;
+
+			system("pause");
+			cout << "thank you to use this program" <<endl;
+			return 0;
+
+
+
+
+
+
+
+
+}
